7bc3fc5da4d0227bdd2a5e07ec3e83187d095522
Tijn <tijnvanbommel@teamrembrandts.com>
2022-01-26 10:48:44 +0100

    checking if git works

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 from scipy.io import wavfile # get the api
 import numpy as np
-
+#check git
 fs, data = wavfile.read('audio/good_things.wav')  # load the data
 a = data.T[0] # this is a two channel soundtrack, I get the first track
 b=[(ele/2**16.)*2-1 for ele in a]  # this is 16-bit track, b is now normalized on [-1,1)
